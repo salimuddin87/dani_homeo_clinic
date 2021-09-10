4b3076950e3e6411eeb0a8c6976df69468a7100d
Salimuddin Ansari <md.salimuddin.ansari@spglobal.com>
2021-09-10 18:10:04 +0530

    test for a model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -22,3 +26,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
