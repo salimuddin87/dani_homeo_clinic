2e9067ab940f26601372b13e98d99fc95eb36416
Salimuddin Ansari <md.salimuddin.ansari@spglobal.com>
2021-09-14 08:19:05 +0530

    static file added

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# Forms with dozens of fields, split the form up into fieldsets
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
